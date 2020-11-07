44bc7a62379094fff4399a867cb8c64a4cdab63b
secretstar919 <bivolmariusdev@gmail.com>
2020-11-07 17:02:06 +0000

    remove refernce to Config class

--- cogs/cheese.py
+++ cogs/cheese.py
@@ -1,7 +1,6 @@
 """This is a cog for a discord.py bot.
 It drops random cheese for people to pick up
 """
-from utils.config import Config
 from collections import defaultdict
 from datetime import datetime as dt
 from discord import Activity, Client, DMChannel, Embed, Message
@@ -13,37 +12,42 @@ import random
 
 class Cheese(commands.Cog, command_attrs=dict(hidden=True)):
 
-    def __init__(self, client):
+    def __init__(self, client, **kwargs):
         self.client = client
         self.client.log.info("loading ze cheese!")
-        self.config = Config('cheese_config.json', no_template=True, live=True)
-        self.DEBUG = self.config.get("debug", False)
-        self.cheese_emoji = u"\U0001F9C0"
-        self.thumbup_emoji = u"\U0001F44D"
-        self.thumbdown_emoji = u"\U0001F44E"
+        #Config
+        self.config = dict()
+        self.config['debug'] = kwargs.get("debug", False)
+        self.config['store_file'] = kwargs.get("store_file",'cheese_store.json')
+        self.config['cheese_weight'] = kwargs.get("cheese_weight", 30)
+        self.cheese_weight = (100 - self.config['cheese_weight'], 100)
+        #Emoji Storage in Unicode
+        self.emojis = dict()
+        self.emojis['cheese_emoji'] = u"\U0001F9C0"
+        self.emojis['thumbup_emoji'] = u"\U0001F44D"
+        self.emojis['thumbdown_emoji'] = u"\U0001F44E"
+        #Timer between cheese drops
         self.last_cheese = dt.utcnow()
-        self.client.log.info(self.config)
-        cheese_weight = self.config.get("cheese_weight", 30)
-        self.cheese_weight = (100 - cheese_weight, 100)
-        self.cooldown = 30
-        self.store_file = 'cheese_store.json'
-        self.scores = self.load_memory()
+        self.cooldown = 30        
+        #Initialize the score memory
+        self.scores_store = self.load_memory()
+        #Warm up the randomizer
         random.seed()
 
     async def save_memory(self):
         try:
-            with open(self.store_file, 'w', encoding='utf-8') as f:
-                json.dump(dict(self.scores), f)
+            with open(self.config['store_file'], 'w', encoding='utf-8') as f:
+                json.dump(dict(self.scores_store), f)
         except Exception as e:
             self.client.log.warning(f"Unable to save cheese memory! : {e}")
         finally:
-            if self.DEBUG:
+            if self.config['debug']:
                 self.client.log.info(f"{await self.list_current_store_users()}")
 
     async def list_current_store_users(self, limit=5):
         output = []
         counter = 1
-        for k, v in sorted(self.scores.items(), key=lambda x: x[1], reverse=True):
+        for k, v in sorted(self.scores_store.items(), key=lambda x: x[1], reverse=True):
             output.append(f"{counter}. {await self.client.fetch_user(int(k))}: {v}")
             if counter > limit:
                 break
@@ -52,7 +56,7 @@ class Cheese(commands.Cog, command_attrs=dict(hidden=True)):
 
     def load_memory(self):
         try:
-            with open(self.store_file, 'r', encoding='utf-8') as f:
+            with open(self.config['store_file'], 'r', encoding='utf-8') as f:
                 scores = defaultdict(int)
                 scores.update(json.load(f))
                 return scores
@@ -64,23 +68,23 @@ class Cheese(commands.Cog, command_attrs=dict(hidden=True)):
     async def add_cheese(self, client: Client, msg: Message):
         message = 'A wild cheese appeared!'
         await msg.channel.send(message)
-        await msg.add_reaction(self.cheese_emoji)
+        await msg.add_reaction(self.emojis['cheese_emoji'])
         await msg.channel.send(await self.check_reaction(client, msg))
 
     async def check_reaction(self, client: Client, msg: Message):
         def check(reaction, user):
             return not user.bot \
                 and msg.id == reaction.message.id \
-                and str(reaction.emoji) == self.cheese_emoji
+                and str(reaction.emoji) == self.emojis['cheese_emoji']
         message_store = ""
         try:
             reaction, user = await client.wait_for('reaction_add', timeout=60.0, check=check)
-            self.scores[str(user.id)] += 1
+            self.scores_store[str(user.id)] += 1
             await self.save_memory()
-            message_store += f"{self.thumbup_emoji} {user} collected the cheese!"
+            message_store += f"{self.emojis['thumbup_emoji']} {user} collected the cheese!"
             return message_store
         except asyncio.TimeoutError:
-            message_store += f"{self.thumbdown_emoji} nobody collected the cheese"
+            message_store += f"{self.emojis['thumbdown_emoji']} nobody collected the cheese"
             return message_store
 
     @commands.Cog.listener()
